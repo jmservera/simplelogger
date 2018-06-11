f620af023304928c339116edbef5cfbb7efb9223
Juan Manuel Servera <jmservera@outlook.com>
2018-06-11 13:05:55 +0200

    appinsights

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var http = require('http');
 var fs = require('fs');
+let appInsights = require('applicationinsights');
+
 var index = fs.readFileSync('index.html');
 var port = process.env.PORT || 1337;
-
+console.log("Start appInsights");
+try {
+    appInsights.setup().start();
+}
+catch(e) {
+    console.error(e);
+}
 console.log(`Starting server in port ${port}. Open a browser in http://localhost:${port}`);
 
 http.createServer(function (req, res) {

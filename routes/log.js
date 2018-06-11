443eeb007a99cb78964c656ff7fb92d86a9355a1
Juan Manuel Servera <jmservera@outlook.com>
2018-06-11 18:03:53 +0200

    find ipaddress from forward

--- routes/log.js
+++ routes/log.js
@@ -5,12 +5,33 @@ var fs = require("fs");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  data=`${new Date().toISOString()};${req.ip};"${req.headers["user-agent"]}"\n`;
-    console.info(req);
+  data=`${new Date().toISOString()};${getClientIp(req)};"${req.headers["user-agent"]}"\n`;
     fs.appendFile('./calls.log', data, (err) => {
         if (err) throw err;
       });
     res.send("OK");
 });
+
+// snippet taken from http://catapulty.tumblr.com/post/8303749793/heroku-and-node-js-how-to-get-the-client-ip-address
+function getClientIp(req) {
+  var ipAddress;
+  // The request may be forwarded from local web server.
+  var forwardedIpsStr = req.header('x-forwarded-for'); 
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    // If request was not forwarded
+    ipAddress = req.connection.remoteAddress;
+  }
+  if(!ipAddress){
+    ipAddress = req.ip;
+  }
+  return ipAddress;
+};
 
 module.exports = router;

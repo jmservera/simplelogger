1bba5214cd95019dd0326d9aec71bcd03782aa33
Juan Manuel Servera <jmservera@outlook.com>
2018-06-11 13:33:47 +0200

    test console.log

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ catch(e) {
 console.log(`Starting server in port ${port}. Open a browser in http://localhost:${port}`);
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
+    console.log("Serve index.html");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
 }).listen(port);
